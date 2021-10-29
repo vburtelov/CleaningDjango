a76b606b2af21d4482ac3324109a8f88076db278
vburtelov <vburtelov@gmail.com>
2021-10-29 22:35:18 +0300

    register models in admin.py

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from api.models import User, Order
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = {'id', 'surname', 'name', 'middlename'}
+
+
+admin.site.register(User)
+admin.site.register(Order)

07b379c94124261e97a01d3c42226d089eb45da7
vburtelov <vburtelov@gmail.com>
2021-10-29 22:36:57 +0300

    create userviewset

--- api/views.py
+++ api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from api.models import User
+from api.serializers import UserSerializer
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    permission_classes = [
+        permissions.AllowAny
+    ]
+    serializer_class = UserSerializer

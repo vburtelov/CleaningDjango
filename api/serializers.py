9a484c329d3370fce37b67f793221fdca207b947
vburtelov <vburtelov@gmail.com>
2021-10-30 20:13:06 +0300

    added order serializer

--- api/serializers.py
+++ api/serializers.py
@@ -6,3 +6,9 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = '__all__'

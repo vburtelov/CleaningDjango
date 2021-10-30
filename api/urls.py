1cf705138f9769393c580dbec6af806a8c953366
vburtelov <vburtelov@gmail.com>
2021-10-30 20:13:29 +0300

    registered one more api link

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,8 @@
 from rest_framework import routers
-from api.views import UserViewSet
+from api.views import UserViewSet, OrderViewSet
 
 router = routers.DefaultRouter()
 router.register('api/user', UserViewSet, 'user')
-
+router.register('api/order', OrderViewSet, 'order')
 
 urlpatterns = router.urls

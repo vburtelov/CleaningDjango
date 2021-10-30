13179a189619f3a10c32ec688c1f87e3aa717878
vburtelov <vburtelov@gmail.com>
2021-10-30 20:12:16 +0300

    added 3 admin models

--- api/admin.py
+++ api/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
-from api.models import User, Order
+from api.models import User, Order, Cleaner
 
 
+@admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = {'id', 'surname', 'name', 'middlename'}
+    list_display = ('id', 'surname', 'name', 'middlename',)
 
 
-admin.site.register(User)
-admin.site.register(Order)
+@admin.register(Cleaner)
+class CleanerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'number_of_sweeps', 'rating',)
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'date', 'address', 'type', 'square', 'cleaner',)

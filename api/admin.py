4dceeaff1f3207850ca7b0ab93f4e7533199ea41
vburtelov <v@burtelov.ru>
2022-07-07 15:58:24 +0300

    Change user model at admin panel

--- api/admin.py
+++ api/admin.py
@@ -2,11 +2,6 @@ from django.contrib import admin
 from api.models import User, Order, Cleaner, ExtraService, DiscountCode, TypeOfCleaning, Frequency
 
 
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'surname', 'name', 'middle_name', 'email',)
-
-
 @admin.register(Cleaner)
 class CleanerAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'number_of_sweeps', 'rating',)

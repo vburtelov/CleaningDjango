56f3b5bf68a0fc95f0bb09ba1a9fef12c9f17d52
vburtelov <v@burtelov.ru>
2022-07-07 19:07:01 +0300

    added admin actions

--- api/admin.py
+++ api/admin.py
@@ -1,16 +1,35 @@
 from django.contrib import admin
-from api.models import User, Order, Cleaner, ExtraService, DiscountCode, TypeOfCleaning, Frequency, BasicService, \
+from api.models import Order, Cleaner, ExtraService, DiscountCode, TypeOfCleaning, Frequency, BasicService, \
     CleanerCalendar, CleaningTime
 
 
 @admin.register(Cleaner)
 class CleanerAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'number_of_sweeps', 'rating',)
+    list_display = ('id', 'name', 'number_of_sweeps', 'rating', 'is_active',)
+    actions = ['make_not_active', 'make_active']
+
+    @admin.action(description='Mark selected cleaners as not active')
+    def make_not_active(self, request, queryset):
+        queryset.update(is_active=False)
+
+    @admin.action(description='Mark selected cleaners as active')
+    def make_active(self, request, queryset):
+        queryset.update(is_active=True)
 
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('id', 'date', 'address', 'type', 'square', 'cleaner',)
+    list_display = ('id', 'date', 'address', 'type', 'square', 'cleaner', 'is_active',)
+
+    actions = ['make_not_active', 'make_active']
+
+    @admin.action(description='Mark selected order as not active')
+    def make_not_active(self, request, queryset):
+        queryset.update(is_active=False)
+
+    @admin.action(description='Mark selected order as active')
+    def make_active(self, request, queryset):
+        queryset.update(is_active=True)
 
 
 @admin.register(CleaningTime)
@@ -30,7 +49,7 @@ class ExtraServiceAdmin(admin.ModelAdmin):
 
 @admin.register(ExtraService)
 class ExtraServiceAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'price_per_meter',)
+    list_display = ('id', 'name', 'price',)
 
 
 @admin.register(TypeOfCleaning)
@@ -46,3 +65,13 @@ class FrequencyAdmin(admin.ModelAdmin):
 @admin.register(DiscountCode)
 class DiscountCodeAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'code', 'is_active',)
+
+    actions = ['make_not_active', 'make_active']
+
+    @admin.action(description='Mark selected discount codes as not active')
+    def make_not_active(self, request, queryset):
+        queryset.update(is_active=False)
+
+    @admin.action(description='Mark selected discount codes as active')
+    def make_active(self, request, queryset):
+        queryset.update(is_active=True)

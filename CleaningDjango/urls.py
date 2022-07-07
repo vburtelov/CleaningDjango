5d7a2bb3ce3b97db78556315de997241fa56c233
vburtelov <v@burtelov.ru>
2022-07-07 16:01:12 +0300

    include users path

--- CleaningDjango/urls.py
+++ CleaningDjango/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('api.urls')),
+    path('api/', include('api.urls')),
+    path('users/', include('users.urls')),
     path('admin/', admin.site.urls),
 ]

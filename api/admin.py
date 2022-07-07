6d8dd3b9a11025165e490045510896df1303f4de
vburtelov <v@burtelov.ru>
2022-07-07 03:47:11 +0300

    Added new models to admin panel

--- api/admin.py
+++ api/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from api.models import User, Order, Cleaner
+from api.models import User, Order, Cleaner, ExtraService, DiscountCode, TypeOfCleaning, Frequency
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'surname', 'name', 'middlename',)
+    list_display = ('id', 'surname', 'name', 'middle_name', 'email',)
 
 
 @admin.register(Cleaner)
@@ -15,3 +15,23 @@ class CleanerAdmin(admin.ModelAdmin):
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('id', 'date', 'address', 'type', 'square', 'cleaner',)
+
+
+@admin.register(ExtraService)
+class ExtraServiceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price_per_meter',)
+
+
+@admin.register(TypeOfCleaning)
+class TypeOfCleaningAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price_per_meter',)
+
+
+@admin.register(Frequency)
+class FrequencyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+
+
+@admin.register(DiscountCode)
+class DiscountCodeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'code', 'is_active',)

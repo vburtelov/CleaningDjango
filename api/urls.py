e2de6a0edaced23029d6e7f5798e4af2e6680494
vburtelov <v@burtelov.ru>
2022-07-07 16:00:49 +0300

    fixed urls to user & order viewsets

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,7 @@ from rest_framework import routers
 from api.views import UserViewSet, OrderViewSet
 
 router = routers.DefaultRouter()
-router.register('api/user', UserViewSet, 'user')
-router.register('api/order', OrderViewSet, 'order')
+router.register('user', UserViewSet, 'user')
+router.register('order', OrderViewSet, 'order')
 
 urlpatterns = router.urls

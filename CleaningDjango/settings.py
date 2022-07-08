371306367c7d5a546b21a727ba50bed9c4840986
vburtelov <v@burtelov.ru>
2022-07-08 14:49:55 +0300

    fixed allowed hosts

--- CleaningDjango/settings.py
+++ CleaningDjango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$$c*x4askic=2lnv#32^qq4+om&%6u3il#3l#tm5bvg5+s5&*y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['cleaning.std-1887.ist.mospolytech.ru', '127.0.0.1:8000', ]
+ALLOWED_HOSTS = ['cleaning.std-1887.ist.mospolytech.ru', '127.0.0.1', ]
 
 # Application definition
 

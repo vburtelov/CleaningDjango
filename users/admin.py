a115fa3ec920a426e56fa7601a8072e8995e8b09
vburtelov <v@burtelov.ru>
2022-07-08 16:30:13 +0300

    added filters & search fields

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
+
 from users.models import CustomUser, CustomGroup
 from django.contrib.auth.models import Group
 
@@ -7,11 +9,13 @@ admin.site.unregister(Group)
 
 
 @admin.register(CustomGroup)
-class GroupAdmin(admin.ModelAdmin):
+class GroupAdmin(ImportExportModelAdmin):
     list_display = ('id', 'name')
 
 
 @admin.register(CustomUser)
-class CustomUserAdmin(admin.ModelAdmin):
+class CustomUserAdmin(ImportExportModelAdmin):
     list_display = ('id', 'name', 'surname', 'email', 'is_superuser',)
+    list_filter = ('is_superuser', 'is_staff', )
+    search_fields = ('name', 'surname', 'email',)
     ordering = ('email',)

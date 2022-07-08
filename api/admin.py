6149532d6b4c43739a12835e5379b04db468d959
vburtelov <v@burtelov.ru>
2022-07-08 16:17:08 +0300

    added search fields & list filters

--- api/admin.py
+++ api/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
+
 from api.models import Order, Cleaner, ExtraService, DiscountCode, TypeOfCleaning, Frequency, BasicService, \
     CleanerCalendar, CleaningTime
 
 
 @admin.register(Cleaner)
-class CleanerAdmin(admin.ModelAdmin):
+class CleanerAdmin(ImportExportModelAdmin):
     list_display = ('id', 'name', 'number_of_sweeps', 'rating', 'is_active',)
+    list_filter = ('rating', 'is_active',)
+    search_fields = ('name', 'rating',)
     actions = ['make_not_active', 'make_active']
 
     @admin.action(description='Mark selected cleaners as not active')
@@ -18,9 +22,10 @@ class CleanerAdmin(admin.ModelAdmin):
 
 
 @admin.register(Order)
-class OrderAdmin(admin.ModelAdmin):
+class OrderAdmin(ImportExportModelAdmin):
     list_display = ('id', 'date', 'address', 'type', 'square', 'cleaner', 'is_active',)
-
+    list_filter = ('date', 'type', 'square', 'cleaner', 'is_active',)
+    search_fields = ('address',)
     actions = ['make_not_active', 'make_active']
 
     @admin.action(description='Mark selected order as not active')
@@ -33,39 +38,47 @@ class OrderAdmin(admin.ModelAdmin):
 
 
 @admin.register(CleaningTime)
-class OrderAdmin(admin.ModelAdmin):
+class OrderAdmin(ImportExportModelAdmin):
     list_display = ('id', 'time',)
 
 
 @admin.register(CleanerCalendar)
-class CleanerCalendarAdmin(admin.ModelAdmin):
+class CleanerCalendarAdmin(ImportExportModelAdmin):
     list_display = ('id', 'cleaner', 'date', 'time', 'order',)
+    list_filter = ('cleaner', 'date', 'time', 'order',)
 
 
 @admin.register(BasicService)
-class ExtraServiceAdmin(admin.ModelAdmin):
+class ExtraServiceAdmin(ImportExportModelAdmin):
     list_display = ('id', 'name',)
+    search_fields = ('name',)
 
 
 @admin.register(ExtraService)
-class ExtraServiceAdmin(admin.ModelAdmin):
+class ExtraServiceAdmin(ImportExportModelAdmin):
     list_display = ('id', 'name', 'price',)
+    search_fields = ('name',)
+    list_filter = ('price',)
 
 
 @admin.register(TypeOfCleaning)
-class TypeOfCleaningAdmin(admin.ModelAdmin):
+class TypeOfCleaningAdmin(ImportExportModelAdmin):
     list_display = ('id', 'name', 'price_per_meter',)
+    search_fields = ('name',)
+    list_filter = ('price_per_meter',)
 
 
 @admin.register(Frequency)
-class FrequencyAdmin(admin.ModelAdmin):
+class FrequencyAdmin(ImportExportModelAdmin):
     list_display = ('id', 'name',)
+    search_fields = ('name',)
 
 
 @admin.register(DiscountCode)
-class DiscountCodeAdmin(admin.ModelAdmin):
+class DiscountCodeAdmin(ImportExportModelAdmin):
     list_display = ('id', 'name', 'code', 'is_active',)
-
+    search_fields = ('name', 'code')
+    list_filter = ('number_of_uses', 'is_active')
     actions = ['make_not_active', 'make_active']
 
     @admin.action(description='Mark selected discount codes as not active')
